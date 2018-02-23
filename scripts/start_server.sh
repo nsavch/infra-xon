82bb5eff6a88fcbb48fc50b6f88504b8f9daa75a
Nick Savchenko <nsavch@gmail.com>
2018-02-23 17:30:37 +0000

    Fixes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,23 +3,16 @@
 set -x
 base_dir=`dirname $0`
 tmpdir=/var/run/xonotic
-server_dir=$base_dir/../servers/$1
+server_dir=$base_dir/xonotic/data_$1
 pidfile=$tmpdir/$1.pid
 stdin_pipe=$tmpdir/$1.fifo
 xonotic_dir=/home/xonotic/xonotic
-xonotic_cmd="${xonotic_dir}/darkplaces/darkplaces-dedicated -xonotic -userdir ${base_dir}/../xonotic"
+xonotic_cmd="${xonotic_dir}/all run dedicated -userdir ${base_dir}/../xonotic"
 
 if [ -z $1 ]
 then
 echo Sever name must be specified
 exit 1
-fi
-
-
-if [ ! -d $server_dir ]
-then
-echo Server $1 does not exist
-exit 2
 fi
 
 rm -f $stdin_pipe
--- scripts/symlink_dl.sh
+++ scripts/symlink_dl.sh
@@ -2,10 +2,9 @@
 
 DL_DIR=/var/www/html/maps
 
-for dirname in '/home/xonotic/infra/xonotic'
+for dirname in '/home/xonotic/infra/xonotic /home/xonotic/xonotic/data/'
 do
 find $dirname -name '*.pk3' -exec ln -sf {} $DL_DIR \;
-find $dirname -name '*.dat' -exec ln -sf {} $DL_DIR \;
 done
 
 find $DL_DIR -xtype l -exec rm {} \;
